6a3ad01946066311f96aac3d89f9ac0a577cf1b0
Suman Kandi <sumankandisuman@gmail.com>
2025-01-14 20:13:58 +0530

    Successful

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const chat = require("./models/chat.js");
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -20,11 +21,36 @@ async function main() {
 }
 
 // index Route
-
 app.get("/chats", async (req, res) => {
     let chats = await chat.find();
     res.render("index.ejs", { chats });
 });
+
+//New Route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+//Create Route
+app.post("/chats", (req, res) => {
+    let { from, to, message } = req.body;
+    let newChat = new chat({
+        from: from,
+        to: to,
+        message: message,
+        created_at: new Date()
+    });
+    newChat.save()
+        .then((res) => {
+            console.log("chat was saved");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    res.redirect("/chats");
+});
+
+
 
 app.get("/", (req, res) => {
     res.send("exp");
